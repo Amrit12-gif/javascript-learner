7712d496784d12f3c95396947f220ecb59a46244
Amrit12-gif <amritreule555@gmail.com>
2025-02-17 03:29:19 +0000

    configure done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,14 @@
-console.log("hitesh")
+const accountId = 12212
+let name = "tommy"
+var password = "12er"
+let email = "@gmail.com"
+let Country;
+
+
+// accountId = "121212121"
+name = "mom"
+password = "fdsfdsioi3"
+email = "jok@gmail.com"
+Country = "bad"
+// console.log(accountId);
+console.table( typeof[accountId, name, password, email, Country])
