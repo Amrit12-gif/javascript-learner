6b2d694274af15db3b324a96f106c526ec389b72
Amrit12-gif <amritreule555@gmail.com>
2025-02-17 17:18:38 +0000

    connection done

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -45,3 +45,50 @@ console.log(typeof strNumber)
 
 
 
+
+/// ********* Operations  ********
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue)
+// console.log(typeof negvalue)
+
+
+// console.log(2+2)
+// console.log(2*6)
+// console.log(2 - 6)
+// console.log(4/2)
+// console.log(5%8)  //remainder
+// console.log(3**2)  // power
+
+
+// ******* add the value *****
+// let str1 = "hello Nepal"
+// let str2 = 12121
+// let str3 = str1  + str2
+// console.log(str3)
+
+
+// console.log(1 + "2")
+// console.log("2" + 1)
+// console.log( "1" + "1")
+// console.log(1 + "2" + "2")
+// console.log("2" + "2" + 1)
+// console.log(1 + 2 + "2")
+
+
+// console.log( (30 + 2) * 2);
+
+
+console.log(+true)
+console.log(+ "")
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 1 + 3;
+console.log(num1)
+
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter)
