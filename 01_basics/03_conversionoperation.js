ac6044cecae240c4cd264cb420c474daf17ef7cc
Amrit12-gif <amritreule555@gmail.com>
2025-02-17 11:50:09 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,47 @@
+let score = true
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+
+// console.log(valueInNumber)
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true = 1, false = 0;
+
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+console.log( typeof booleanIsLoggedIn)
+
+// 1 = true, false = 0;
+// "" = false
+// "hitesh" = true
+
+
+
+
+let num = 444
+let stringNumber = String(num)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
+
+
+let str = "222"
+let strNumber = Number(str)
+console.log(strNumber)
+console.log(typeof strNumber)
+
+
+
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,12 +1,3 @@
-// console.log(3+3); console.log( typeof "hammer")
+let score = 33
 
 
-
-let name = "ham"
-let age = 1323
-let isLoggedIn = true
-let x; // undefined
-let city = null   // nulll
-
-console.log(typeof city)
-// console.table([name,age, isLoggedIn, x, city])
