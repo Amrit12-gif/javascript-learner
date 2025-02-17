3264808fd0cf89dc1dbae210790463f14c82190c
Amrit12-gif <amritreule555@gmail.com>
2025-02-17 04:15:26 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use stict";   //treat all js code as newer version 
+
+// alert(3 + 3)    // we are using nodejs, not browser
+
+console.log(3
+    +
+    3);  // code readability should be high
+// console.log("rammm")
+
+
+let name = "hari"
+let age = 23
+let isLoggedIn = false
+let state;
+
+
+
+// number => 1 t0 power 53 
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => 
+//symbol => unique
+
+// object => 
+
+
+    console.log(typeof undefined);  //undefined
+    console.log(typeof null); //object
+    
