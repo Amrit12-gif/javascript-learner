f71c0bb2ff3249cb0f5c0b6d91090bcca96427fd
Amrit12-gif <amritreule555@gmail.com>
2025-02-20 13:47:07 +0000

    solve

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,26 +1,5 @@
-"use strict"  // treat all js code as newer version 
-
-// alert(3+2)  // we are using nodejs, not browser
-// console.log(3  // code with readability should be high
-//     + 3
-// )
-
-
-let name = "anu"
-let age = 1212
-let isLoggedIn = false;
-let state;
-
-// console.log(age)
-// console.log( typeof age)
-
-// console.log(isLoggedIn)
-// console.log( typeof isLoggedIn)
-
-console.log(state)
-console.log( typeof state)
-
-
-
-
-
+console.log( 2 > 2)
+console.log(2 >= 2)
+console.log(2 == 2)
+console.log(2 != 0)
+console.log("1" > 1)
