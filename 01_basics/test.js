35eb36245cfe9f8b2ab50a47d07c82d153217e50
Amrit12-gif <amritreule555@gmail.com>
2025-02-20 15:16:08 +0000

    datatypes-summary

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,5 +1,19 @@
-console.log( 2 > 2)
-console.log(2 >= 2)
-console.log(2 == 2)
-console.log(2 != 0)
-console.log("1" > 1)
+// let information = ["home", "sonu", 1213, ]
+// console.log(information)
+
+
+
+
+// Object
+// let myname = {
+//     name: "joker",
+//     age: 33,
+//     email: "@isfs"
+// }
+// console.log(myname)
+// console.log(typeof myname)
+
+let myfunction = function(){
+console.log("hello my It carries")
+
+}
---
+++ datatypes-summary.js
@@ -0,0 +1,45 @@
+// # primitive 
+
+//  7 types: String, Number, Boolean, Null, Symbol, undefined, BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false;
+const outside = null;
+let userEmail;
+
+const id  = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId)
+
+const bigNumber = 5451254512154451255n 
+
+// Reference (Non primitive)
+
+// Array, Objects, functions
+
+/// *********** Array ******* \\\
+const identity = ["Suman", "imadol", "it@gmail.com" ,"57309475"]
+
+
+
+/// ****** Object  ****///
+let myObj = {
+    name: "hari",
+    Rollno: 12,
+    email: "top@gmail.com"
+
+}
+
+
+// ****** funcion *******
+const myfunction = function(){
+ console.log("i am a javascript");   
+}
+
+
+
+
+
+// console.log( myfunction )
+// console.log(typeof )
