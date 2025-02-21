702386212e62e9794c600bcfd8d41b2a8562b041
Amrit12-gif <amritreule555@gmail.com>
2025-02-21 15:00:07 +0000

    primitive

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,19 +1,56 @@
-// let information = ["home", "sonu", 1213, ]
-// console.log(information)
+//**** javascript is a dynamic programming language and it is changeable  */
+// primitive datatypes(call by value) 
+// 7types= string, number, boolean,  null, undefined, symbol, BigInt
+
+// let str = "anup"
+// let num = 1212
+
+//  let myname;
+//  let BigInt = 133232323223232n;
+
+
+//  console.log(myname)
+//  console.log(typeof myname)
+
+
+// const score = 100;
+// const scorevalue = 100.33;
+// const isLoggedIn = false;
+// const outsidetemp = null;
+// let useremail;
+
+// const emailid = Symbol ('@gmail')
+// const id =  Symbol ('@gmail')
+// console.log(emailid === id)
+
+// const bignum = 121343343n;
+// console.log(bignum)
+// console.log(typeof bignum)
 
 
 
 
-// Object
-// let myname = {
-//     name: "joker",
-//     age: 33,
-//     email: "@isfs"
+
+
+///  nonprimitive(call by reference)
+// Array, Object, function
+
+// let  mydetails =["ram", "@gmail"]
+// console.log(mydetails)
+// console.log(typeof mydetails)
+
+
+// let myinform = {
+//     name:"Amrit",
+//     age: 12,
+//     email: "2gmail.com",
 // }
-// console.log(myname)
-// console.log(typeof myname)
+// console.log(myinform)
+// console.log(typeof myinform)
 
-let myfunction = function(){
-console.log("hello my It carries")
 
-}
+const myfunction = function(){
+    console.log("i am a javascript");   
+   }
+   
+
