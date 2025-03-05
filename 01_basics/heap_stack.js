257ed032cba6f943a293da699ec312dab3e199ca
Amrit12-gif <amritreule555@gmail.com>
2025-03-05 17:54:21 +0000

    heap

---
+++ 01_basics/heap_stack.js
@@ -0,0 +1,34 @@
+// stack is a primitive memory => string, number, float, integer etc.
+
+// let myyoutubename = "AmritReule"
+//  let anothername = "codechannel"
+
+// console.log(anothername)
+// console.log( myyoutubename)
+
+// function fun() {
+//     let name = "Anjan Rai";
+//     console.log("back last" + name);
+
+// }
+// fun();
+
+
+
+
+
+
+
+// Heap is a non primitive memory => Objects, Array, Function, etc.
+
+
+let person ={
+    name:"mom",
+    age: 22
+};
+
+let person2 = person;
+person.age = 23;
+console.log(person2.age)
+
+
