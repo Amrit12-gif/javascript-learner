f00d05cb8f6cc12f8e6c02469a888825d6f3a3eb
Amrit12-gif <amritreule555@gmail.com>
2025-03-08 09:23:18 +0000

    config

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,26 +1,60 @@
-const name = "look at mee sir";
-const rollno = 30;
-const email = "Hacker@gmail";
-
-// console.log(name + rollno + email);
-console.log(`my name is ${name}, my rollno is ${rollno}, my email is ${email}`);
-
-const mynames = new String("kaplana-ch")
-// console.log(mynames[3])
-// console.log(mynames.__proto__)
-// console.log(mynames.length)
-// console.log(mynames.toUpperCase())
-// console.log(mynames.toLowerCase());
-
-console.log(mynames.charAt(4))
-console.log(mynames.indexOf('n'))
+//  Single string
+console.log('single quoted')
 
 
-const newString = mynames.substring(0, 6)
-console.log(newString)
+// Double String
+console.log("Double Quoted")
 
+// backtickQuoted
+console.log(`backtickquoted`)
+
+
+//****  concatation */
+const name = "man";
+const age = 22;
+const password = 12121;
+console.log(name + age + password);
+console.log(name + age + password, "i am a concat string");
+
+// STRING DECLARE
+const gameName = new String('Amrit-reule')
+console.log(gameName[1]); 
+console.log(gameName.__proto__);
+console.log( typeof gameName.__proto__);
+
+
+
+console.log(gameName.length)
+console.log(gameName.toUpperCase());
+console.log(gameName.toLocaleLowerCase());
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('r'))
+
+
+// substring
+const myname = gameName.substring(0,7)
+console.log(myname)
 
 
 // slice
-const anotherString = mynames.slice(-1, 5)
-console.log(anotherString)
+const anothername = gameName.slice(-9, 0)
+console.log(anothername)
+
+// trim() = used to space join
+const newspace = "    Amrit    ";
+console.log(newspace);
+console.log(newspace.trim());
+
+// replace
+const tom = "reuleamrit34@.com";
+console.log(tom.replace('reule', 'hari'))
+
+// includes
+console.log(tom.includes('reuleamrit'))
+// console.log(tom.includes('mom'))
+
+// split
+let spt = ('name, top, room, llok')
+console.log(spt.split("*"));
+
+
